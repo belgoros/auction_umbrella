a8d5e5e4953c6dceefe6cb2661ce37c4cd61bc8a
Serguei Cambour <s.cambour@gmail.com>
2021-07-12 17:21:15 +0200

    Implement Item creation with errors display

--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -1,7 +1,7 @@
 defmodule Auction do
-  alias Auction.Item
+  alias Auction.{Repo, Item}
 
-  @repo Auction.Repo
+  @repo Repo
 
   def list_items do
     @repo.all(Item)
@@ -16,8 +16,8 @@ defmodule Auction do
   end
 
   def insert_item(attrs) do
-    Auction.Item
-    |> struct(attrs)
+    %Item{}
+    |> Item.changeset(attrs)
     |> @repo.insert()
   end
 
--- apps/auction_web/lib/auction_web/controllers/item_controller.ex
+++ apps/auction_web/lib/auction_web/controllers/item_controller.ex
@@ -11,6 +11,13 @@ defmodule AuctionWeb.ItemController do
     render(conn, "new.html", item: item)
   end
 
+  def create(conn, %{"item" => item_params}) do
+    case Auction.insert_item(item_params) do
+      {:ok, item} -> redirect(conn, to: Routes.item_path(conn, :show, item))
+      {:error, item} -> render(conn, "new.html", item: item)
+    end
+  end
+
   def show(conn, %{"id" => id}) do
     item = Auction.get_item(id)
     render(conn, "show.html", item: item)
