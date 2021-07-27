b5186441f45dcd432cd0b76562bdbebd128dea86
Serguei Cambour <s.cambour@gmail.com>
2021-07-27 12:49:41 +0200

    Set up Sandbox adapter to use DB in tests

--- apps/auction/test/auction_test.exs
+++ apps/auction/test/auction_test.exs
@@ -1,5 +1,13 @@
 defmodule AuctionTest do
   use ExUnit.Case
+  alias Auction.{Repo}
   doctest Auction
 
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+  end
+
+  test "the truth" do
+    assert true
+  end
 end
